786e8392dda53daf536751bfea45de7fe44ef983
Dan <kcomtdaniel@gmail.com>
2023-01-25 04:48:56 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "mi primer description",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi segunda description",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "mi tercera description",
+  },
+];
